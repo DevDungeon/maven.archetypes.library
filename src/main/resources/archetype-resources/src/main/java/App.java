511216d3ebc06546fa889a397bd4d5fc6875b13e
NanoDano <nanodano@devdungeon.com>
2018-04-25 01:09:14 -0500

    Initial stable commit of archetype

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.devdungeon.maven.archetypes.maven.archetype.library;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
